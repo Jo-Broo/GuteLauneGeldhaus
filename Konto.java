6fbcb20ce57e14d8414312c4701d23f2146e34a0
Jonas Wolf <Jonas.Wolf.1158@gmail.com>
2024-02-05 13:36:54 +0100

    inital commit

---
+++ Konto.java
@@ -0,0 +1,35 @@
+public class Konto {
+    private double kontostand;
+    private String iban;
+    
+    public Konto(String iban) 
+    {
+        this.iban = iban;
+        kontostand = 0;
+    }
+
+    public double getKontostand() 
+    {
+        return kontostand;
+    }
+    
+    public String getIban() 
+    {
+        return iban;
+    }
+
+    public void einzahlen(double betrag) 
+    {
+        kontostand += betrag;
+    }
+
+    public boolean auszahlen(double betrag) 
+    {
+        if(betrag > this.kontostand)
+        {
+            return false;
+        }
+        kontostand -= betrag;
+        return true;
+    }
+}
